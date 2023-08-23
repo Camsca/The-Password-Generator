311fd77579f786fd5c44796df28c1da2512dde63
Camsca <camila_vass@hotmail.com>
2023-08-23 16:07:33 -0700

    conditional statements,funtions

--- Develop/script.js
+++ Develop/script.js
@@ -12,3 +12,14 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+function generatePassword() {
+var passwordLength = prompt("Enter password length (between 8 and 128):");
+if (passwordLength === null) {
+  return;
+}
+// User clicks on "cancel" or closes the promp
+passwordLength = parseInt(passwordLength);
+if (isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128) {
+alert("Invalid password length. Please enter a value between 8 and 128.");
+return;
+}
